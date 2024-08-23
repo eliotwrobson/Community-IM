c66dea8fce16cc926a623eeee716e335bb210f34
Eliot Robson <eliot.robson24@gmail.com>
2024-08-23 10:48:06 -0500

    Simplified graph creation

--- dataset_manager.py
+++ dataset_manager.py
@@ -18,24 +18,21 @@ def process_deezer(file_path: str) -> nx.DiGraph:
             # Skip first line because it's just schema
             next(line_iter)
 
-            deezer_graph = nx.Graph(
+            deezer_graph = nx.from_edgelist(
                 tuple(map(int, line.strip().split(b","))) for line in line_iter
             )
 
-    # print(deezer_graph.number_of_edges(), deezer_graph.number_of_nodes())
     deezer_graph = deezer_graph.to_directed()
     set_activation_weighted_cascade(deezer_graph)
 
     return deezer_graph
 
 
-# TODO I think the exception is happening here? Somewhere in this file at least
 def process_facebook(file_path: str) -> nx.DiGraph:
     with gzip.open(file_path, "r") as f:
-        facebook_graph = nx.Graph()
-        for line in f.readlines():
-            u, v = tuple(map(int, line.split()))
-            facebook_graph.add_edge(u, v)
+        facebook_graph = nx.from_edgelist(
+            tuple(map(int, line.split())) for line in f.readlines()
+        )
 
     facebook_graph = facebook_graph.to_directed()
     set_activation_weighted_cascade(facebook_graph)
@@ -54,13 +51,20 @@ def process_wikipedia(file_path: str) -> nx.DiGraph:
 
             edge_list.append(tuple(map(int, line_list)))
 
-    wikipedia_graph = nx.from_edgelist(edge_list).to_directed()
+    wikipedia_graph = nx.convert_node_labels_to_integers(
+        nx.from_edgelist(edge_list).to_directed()
+    )
     set_activation_weighted_cascade(wikipedia_graph)
 
     return wikipedia_graph
 
 
-def get_graph(dataset_name: str):
+def process_epinions1(file_path: str) -> nx.DiGraph:
+    with gzip.open(file_path, "r") as f:
+        pass
+
+
+def get_graph(dataset_name: str) -> nx.DiGraph:
     DATASETS = {
         "facebook": {
             "link": "https://snap.stanford.edu/data/facebook_combined.txt.gz",
@@ -76,6 +80,11 @@ def get_graph(dataset_name: str):
             "link": "https://snap.stanford.edu/data/deezer_europe.zip",
             "processor": process_deezer,
             "hash": "dd66a73f8d8690b5bc300ba378883fb2c2f6316aec8917b6a2428e352fc9e498",
+        },
+        "Epinions1": {
+            "link": "https://snap.stanford.edu/data/soc-Epinions1.txt.gz",
+            "processor": process_epinions1,
+            "hash": None,
         },
     }
 
