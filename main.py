242d5384725d2d0f37e37617842e203ad1e40b56
Eliot Robson <eliot.robson24@gmail.com>
2024-08-28 17:25:40 -0500

    Getting LIM code to work

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import dataset_manager as dm
-import mle_greedy as mle
+# import dataset_manager as dm
+# import mle_greedy as mle
+import networkx as nx
+
+import lim_im as li
 
 
 def fractional_im_experiments() -> None:
     """
     The goal of these experiments is to run the fractional IM algorithm against various other algorithms.
     """
-    graph = dm.get_graph("youtube")
+    # graph = dm.get_graph("youtube")
 
-    mle.mle_greedy(graph, 10.4)
+    # mle.mle_greedy(graph, 10.4)
+
+    n = 1_000
+    p = 0.01
+
+    graph = nx.gnp_random_graph(n, p)
+
+    li.lim_im(graph, 10)
+    print("Here")
 
 
 def main() -> None:
