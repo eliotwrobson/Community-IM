980fbbc1c70328adcdecfcc7a10fc7d51ad4b706
Eliot Robson <eliot.robson24@gmail.com>
2024-09-04 13:14:01 -0500

    Better experiments

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import networkx as nx
+# import networkx as nx
+import pandas as pd
+import tqdm
 from cynetdiff.utils import networkx_to_ic_model
 
 import dataset_manager as dm
 import frac_influence as fi
 import lim_im as li
-import mle_greedy as mg
 import ris_selection as rs
-
-li
-mg
-dm
 
 
 def fractional_im_experiments() -> None:
@@ -19,32 +16,50 @@ def fractional_im_experiments() -> None:
 
     # Get graph to run experiments
     # TODO run every graph through this.
-    # graph = dm.get_graph("deezer")
+    graph = dm.get_graph("deezer")
 
-    n = 1_000
-    p = 0.01
-    random_seed = 12345
+    # n = 1_000
+    # p = 0.01
+    # random_seed = 12345
 
-    graph = nx.gnp_random_graph(n, p, seed=random_seed)
-    graph.name = "Temp_graph"
+    # graph = nx.gnp_random_graph(n, p, seed=random_seed)
+    # graph.name = "Temp_graph"
 
     # First, run LIM code and get data
-    seeds, influence, times = li.lim_im(graph)
+    lim_seeds, influence, lim_times = li.lim_im(graph)
 
     # Next, do RIS simulation for rounded budget
     # NOTE Hard-coded to 20 because this is hard-coded in the LIM code
-    vertices, runtime = rs.ris_im(graph, 20)
+    vertices, ris_runtime = rs.ris_im(graph, 20)
 
     model, _ = networkx_to_ic_model(graph)
 
-    for seed_dict in seeds:
+    graph_runtime_info = []
+
+    for seed_dict, lim_time in tqdm.tqdm(
+        zip(lim_seeds, lim_times), total=len(lim_seeds)
+    ):
         seed_vals = sorted(seed_dict.values(), reverse=True)
         mle_seed_dict = dict(zip(vertices, seed_vals))
 
         lim_influence = fi.compute_fractional_influence(model, seed_dict)
         mle_influence = fi.compute_fractional_influence(model, mle_seed_dict)
+        budget = sum(seed_vals)
 
-        print(lim_influence, mle_influence)
+        graph_runtime_info.append(
+            {
+                "lim_influence": lim_influence,
+                "lim_runtime": lim_time,
+                "mle_influence": mle_influence,
+                "mle_total_time": ris_runtime,
+                "budget": budget,
+            }
+        )
+
+        # print(lim_influence, mle_influence)
+
+    df = pd.DataFrame(graph_runtime_info)
+    df.to_csv(f"{graph.name}_benchmark_results.csv")
 
 
 def main() -> None:
