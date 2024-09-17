d841534383e418f7b532ef8fb1b46f06d5b2136b
Eliot Robson <eliot.robson24@gmail.com>
2024-09-17 14:43:25 -0500

    Added graphs

--- make_graphs.py
+++ make_graphs.py
@@ -44,7 +44,22 @@ def main() -> None:
             cd_influences.append(float(row[2]))
             cd_runtimes.append(float(row[3]))
 
-    lim_line, mle_line, cd_line = plt.plot(
+    # Next, get the ud runtimes
+    ud_influences = []
+    ud_runtimes = []
+    ud_budgets = []
+
+    with open(
+        result_folder + os.sep + f"{graph_name.lower()}_ud_results.csv"
+    ) as csvfile:
+        row_iter = csv.reader(csvfile)
+        next(row_iter)
+        for row in row_iter:
+            ud_budgets.append(float(row[1]))
+            ud_influences.append(float(row[2]))
+            ud_runtimes.append(float(row[3]))
+
+    lim_line, mle_line, cd_line, ud_line = plt.plot(
         budgets,
         lim_influences,
         "o",
@@ -54,21 +69,25 @@ def main() -> None:
         cd_budgets,
         cd_influences,
         "o",
+        ud_budgets,
+        ud_influences,
+        "o",
         markersize=2,
     )
 
     lim_line.set_label("LIM Influence")
     mle_line.set_label("MLE Influence")
     cd_line.set_label("CD Influence")
+    ud_line.set_label("UD Influence")
 
     plt.ylabel("Influence")
     plt.xlabel("Budget")
-    plt.title(f"LIM vs MLE {graph_name} Influence")
+    plt.title(f"Influence Maximization on {graph_name} Graph")
     plt.grid(True)
     plt.legend()
     plt.show()
 
-    lim_line, mle_line, cd_line = plt.plot(
+    lim_line, mle_line, cd_line, ud_line = plt.plot(
         budgets,
         lim_runtimes,
         "o",
@@ -78,16 +97,21 @@ def main() -> None:
         cd_budgets,
         cd_runtimes,
         "o",
+        ud_budgets,
+        ud_runtimes,
+        "o",
         markersize=2,
     )
 
     lim_line.set_label("LIM Runtime")
     mle_line.set_label("MLE Runtime")
     cd_line.set_label("CD Runtime")
+    ud_line.set_label("UD Runtime")
 
     plt.ylabel("Runtime (s)")
     plt.xlabel("Budget")
-    plt.title(f"LIM vs MLE {graph_name} Runtime")
+    plt.title(f"Influence Maximization {graph_name} Runtimes")
+    plt.yscale("log")
     plt.grid(True)
     plt.legend()
     plt.show()
