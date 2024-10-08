28ff7312af5e8eccf1d03a185041c81e924c09bc
Eliot Robson <eliot.robson24@gmail.com>
2024-10-08 15:57:58 -0500

    Getting the estimator to work

--- community_im_main.py
+++ community_im_main.py
@@ -29,6 +29,33 @@ def reverse_partition(
     return res_dict
 
 
+def compute_community_aware_diffusion_degrees(
+    graph: nx.DiGraph, rev_partition_dict: dict[int, int]
+) -> dict[int, int]:
+    graph_no_community_edges = nx.subgraph_view(
+        graph, filter_edge=lambda u, v: rev_partition_dict[u] != rev_partition_dict[v]
+    )
+
+    # TODO add temp dict here
+
+    for start_node in graph_no_community_edges:
+        for neighbor in graph_no_community_edges.neighbors(start_node):
+            # Add to probability
+            graph_no_community_edges[start_node][neighbor]["activation_prob"]
+
+            for second_neighbor in graph_no_community_edges.neighbors(neighbor):
+                if second_neighbor == start_node:  # Avoid going back to the start node
+                    continue
+
+                graph_no_community_edges[start_node][neighbor]["activation_prob"]
+                graph_no_community_edges[neighbor][second_neighbor]["activation_prob"]
+
+                path = (start_node, neighbor, second_neighbor)
+                print(path)
+
+    return {}
+
+
 def main() -> None:
     """
     Method used in experiments consists of three steps.
@@ -45,12 +72,12 @@ def main() -> None:
     # Next, remove inter-community edges from graph
     rev_partition_dict = reverse_partition(parts)
 
-    graph_edges_removed = nx.subgraph_view(
+    graph_only_community_edges = nx.subgraph_view(
         graph, filter_edge=lambda u, v: rev_partition_dict[u] == rev_partition_dict[v]
     )
-
+    compute_community_aware_diffusion_degrees(graph, rev_partition_dict)
     print(f"Graph number of edges: {graph.number_of_edges()}")
-    print(f"Graph new number of edges: {graph_edges_removed.number_of_edges()}")
+    print(f"Graph new number of edges: {graph_only_community_edges.number_of_edges()}")
 
     # for part in parts:
     #    print(part)
