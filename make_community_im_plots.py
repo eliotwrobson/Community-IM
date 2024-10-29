9f69e158422354f4939aa8623abd413f9db22a50
Eliot Robson <eliot.robson24@gmail.com>
2024-10-29 13:17:27 -0500

    change budgets

--- make_community_im_plots.py
+++ make_community_im_plots.py
@@ -43,7 +43,7 @@ def main() -> None:
     degree_influences = []
     degree_discount_influences = []
 
-    budgets = [5, 20]
+    budgets = [5, 10, 20, 100]
 
     community_im_runtimes = []
     community_im_dd_runtimes = []
